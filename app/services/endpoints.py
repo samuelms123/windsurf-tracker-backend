5b43206911256125c1e8b1d9aecb899411bf8fc3
Samuelms <samuel.sarimo@gmail.com>
2025-09-12 23:58:55 +0300

    Get stream data method

--- app/services/endpoints.py
+++ app/services/endpoints.py
@@ -1,2 +1,3 @@
 AUTH_ENDPOINT:str = "https://www.strava.com/oauth/token"
 ACTIVITIES_ENDPOINT:str = "https://www.strava.com/api/v3/athlete/activities"
+STREAM_ENDPOINT = "https://www.strava.com/api/v3/activities/{id}/streams"
--- app/services/fetch_service.py
+++ app/services/fetch_service.py
@@ -3,11 +3,17 @@ import requests
 from app.services import endpoints
 
 
-def get_activity_data(access_token:str, params:dict=None) -> dict:
+def get_activity_data(access_token:str) -> dict:
     headers:dict = {'Authorization': f'Authorization: Bearer {access_token}'}
-    if not params:
-        response:dict = requests.get(endpoints.ACTIVITIES_ENDPOINT, headers=headers)
-    response:dict = requests.get(endpoints.ACTIVITIES_ENDPOINT, headers=headers, params=params)
+    response = requests.get(endpoints.ACTIVITIES_ENDPOINT, headers=headers)
     response.raise_for_status()
     activity_data = response.json()
     return activity_data
+
+#STREAM_ENDPOINT.format(id=activity_id)
+def get_stream_data(access_token:str, activity_id:int) -> dict:
+    headers:dict = {'Authorization': f'Authorization: Bearer {access_token}'}
+    response = requests.get(endpoints.STREAM_ENDPOINT.format(id=activity_id), headers=headers)
+    response.raise_for_status()
+    stream_data = response.json()
+    return stream_data
