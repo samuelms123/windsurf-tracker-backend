b2276b7fd6687305181c9de9361336b0a7001cae
Samuelms <samuel.sarimo@gmail.com>
2025-10-15 20:20:22 +0300

    Developement of the sync with strava endpoint

--- app/routes/strava_routes.py
+++ app/routes/strava_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.services import strava_service
 from app.models import models
+from datetime import datetime
+from app.services import analysis_service
 
 router = APIRouter(prefix="/strava", tags=["Strava"])
 
@@ -21,17 +23,27 @@ async def get_streams(activity_id: int, access_token: str):
 async def sync_with_strava(access_token: str, username: str):
     # check from database latest synced activity
     latest_sync = models.get_latest_sync_date(username)
+    latest_sync = datetime(2025, 10, 2) # for testing
     
-    # fetch from strava API
-    result = strava_service.get_latest_activities(access_token, latest_sync)
+    # fetch activites from strava API
+    activities = strava_service.get_latest_activities(access_token, latest_sync)
     
     # update latest sync in database
     dbresult = models.set_latest_sync_date(username)
     
+    # return if no new activities
     
-    return result
-    # get activities after x date
-    # if acticities found:
-    # get streams from strava api with the activity ids
-    # do anylysing on the data
-    #return results
+    
+    # extract all the activity id:s (and other info if needed)
+    # straight from api:
+    # Elapsed time(s), average speed(m/s, maxspeed(m/s), avg heartrate, max heartrate, total distance(m), get location from coordinates
+    activity_ids = [activity["id"] for activity in activities]
+    
+    # get streamdata and analyze
+    analysis_results = []
+    for id in activity_ids:
+        data = strava_service.get_stream_data(access_token, id)
+        result = analysis_service.analyze_data(data)
+        analysis_results.append(result)
+    
+    return analysis_results
--- app/services/analysis_service.py
+++ app/services/analysis_service.py
@@ -1,1 +1,10 @@
 ### Data analysis logic
+
+def analyze_data(data):
+    ## split into many steps
+    # top speed during 5 sec intervals
+    # top speed during 10 sec intervals
+    # acceleration from small speed to 10 knots
+    # acceleration from smaal speed to 20 knots
+    # gybes amount
+    pass
