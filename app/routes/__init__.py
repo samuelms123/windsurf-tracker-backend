7443fc4b4fc7381bc07829c9d3f2c06083fe2fe1
Samuel Sarimo <samuel.sarimo@gmail.com>
2026-07-08 14:22:32 +0300

    Add vpn ping endpoint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from .strava_routes import router as strava_router
 from .activity_routes import router as activity_router
+from .health_routes import router as health_router
 
 master_router = APIRouter()
 
 # Group
 master_router.include_router(strava_router)
 master_router.include_router(activity_router)
+master_router.include_router(health_router) 
